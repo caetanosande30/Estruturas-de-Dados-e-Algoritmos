f2b47bb6aecf28782006b87e2307a965a69dbb31
caetanosande30 <caetanosande30@gmail.com>
2026-03-20 08:38:44 +0200

    Implement main function for document queue management

--- CODIGO/main.c
+++ CODIGO/main.c
@@ -1,1 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include "funcoes.h"
 
+int main() {
+    Fila fila;
+    inicializar(&fila);
+
+    int opcao;
+    char nome[50];
+    int prioridade;
+
+    do {
+        printf("\n1 - Adicionar documento\n");
+        printf("2 - Imprimir documento\n");
+        printf("3 - Mostrar fila\n");
+        printf("0 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch (opcao) {
+            case 1:
+                printf("Nome do documento: ");
+                fgets(nome, 50, stdin);
+                nome[strcspn(nome, "\n")] = 0;
+
+                printf("Prioridade (1 = Alta, 0 = Normal): ");
+                scanf("%d", &prioridade);
+                getchar();
+
+                adicionar(&fila, nome, prioridade);
+                break;
+
+            case 2:
+                imprimir(&fila);
+                break;
+
+            case 3:
+                mostrar(&fila);
+                break;
+        }
+
+    } while (opcao != 0);
+
+    return 0;
+}
