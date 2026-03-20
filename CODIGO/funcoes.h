c5d7fae4f20ea200487a08fbfb1c925318a54890
noevilanculos-code <noevilanculos100@gmail.com>
2026-03-20 20:16:28 +0200

    Update funcoes.h

--- CODIGO/funcoes.h
+++ CODIGO/funcoes.h
@@ -1,1 +1,24 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
 
+#define MAX 50
+
+typedef struct Documento {
+    char nome[MAX];
+    int prioridade;
+    struct Documento* prox;
+} Documento;
+
+typedef struct {
+    Documento* inicio;
+    Documento* fim;
+} Fila;
+
+void inicializar(Fila* f);
+Documento* criarDocumento(char nome[], int prioridade);
+
+void adicionar(Fila* f, char nome[], int prioridade);
+void imprimir(Fila* f);
+void mostrar(Fila* f);
+
+#endif
